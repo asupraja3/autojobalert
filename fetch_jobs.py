501d03099ed24a795acdfc911a2bc6fb78ce2d9e
Supraja Aravamudhan <asupraja527@gmail.com>
2025-07-24 19:13:06 -0500

    Refactor fetch_jobs to use fixed API URL

--- fetch_jobs.py
+++ fetch_jobs.py
@@ -1,5 +1,6 @@
 import requests
 
-def fetch_jobs(url):
+def fetch_jobs():
+    url = "https://remotive.io/api/remote-jobs?category=software-dev"
     response = requests.get(url)
     return response.json()["jobs"]
